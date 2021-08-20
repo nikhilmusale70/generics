09f07ac9565764e6ad1bf9075e1b0ebd9b725285
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-21 00:21:25 +0530

    Added Genrics

--- src/genrics.java
+++ src/genrics.java
@@ -1,15 +1,25 @@
 public class genrics {
+
     public static void main(String[] args) {
         String a = new String("Apple");
         String b = new String("Peach");
         String c = new String("Banana");
 
+        Integer d = new Integer(4);
+        Integer e = new Integer(5);
+        Integer f = new Integer(6);
+
+        Float g = new Float(1.1);
+        Float h = new Float(1.2);
+        Float i = new Float(0.9);
+
         genrics gen = new genrics();
         gen.maxNumbers(a,b,c);
-
+        gen.maxNumbers(d,e,f);
+        gen.maxNumbers(g,h,i);
     }
 
-    public void maxNumbers(String a, String b, String c){
+    public <T extends Comparable<T>> void maxNumbers (T a, T b, T c){
         if ((a.compareTo(b) >= 0) && (a.compareTo(c) >= 0)){
                 System.out.println( a + " is greatest");
         }
